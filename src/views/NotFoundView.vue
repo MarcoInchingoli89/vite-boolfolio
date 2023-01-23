a81cb231fff56c59e48262bb613b5a843dd0556b
Marco Inchingoli <evilmarkus@hotmail.it>
2023-01-23 15:52:07 +0100

    Fixed Layout NotFoundView

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -7,14 +7,38 @@ export default {
 </script>
 
 <template>
-    <AppBanner title="404" />
-    <div class="container mt-3">
-        <p class="lead">
-            😨 Ops, Page not found!
-        </p>
-        <router-link to="/">Go to Homepage</router-link>
 
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center row">
+            <div class=" col-md-6">
+                <img src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg" alt=""
+                    class="img-fluid">
+            </div>
+            <div class=" col-md-6 mt-5">
+                <p class="fs-3"> <span class="text-danger">😨 Opps!</span> Page not found.</p>
+                <p class="lead">
+                    The page you’re looking for doesn’t exist.
+                </p>
+                <router-link class="btn btn-dark" to="/">Go to Homepage</router-link>
+            </div>
+
+        </div>
     </div>
+
+
+
+
+
+    <!--  <div class="d-flex align-items-center justify-content-center vh-100 bg-secondary">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-3"> <span class="text-danger">😨 Opps!</span> Page not found.</p>
+            <p class="lead">
+                The page you’re looking for doesn’t exist.
+            </p>
+            <router-link class="btn btn-dark" to="/">Go to Homepage</router-link>
+        </div>
+    </div> -->
 
 </template>
 
