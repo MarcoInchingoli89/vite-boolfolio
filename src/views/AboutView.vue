9313e6117a875daabe299caaf17d9ad2bc2b37b2
Marco Inchingoli <evilmarkus@hotmail.it>
2023-01-27 00:38:10 +0100

    Add image about, add section info about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,33 +9,69 @@ export default {
 <template>
 
     <AppBanner title="About" />
-    <div class="container-fluid pt-4 vh-100 text-white text-center">
-        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti fugiat illo asperiores
-            provident aut incidunt. Voluptate necessitatibus nisi delectus, dolorum reprehenderit accusamus aspernatur
-            saepe sequi doloribus ullam similique aut dolor!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni perspiciatis similique autem error
-            veritatis aut sit molestias inventore id, sapiente omnis numquam labore nobis fuga delectus reiciendis
-            officia velit optio iusto facilis expedita animi. Ipsam, facilis laborum aperiam, dolorem quo molestias
-            repellendus aliquid quidem commodi, ipsum asperiores sequi cumque recusandae repudiandae eius eos. Illum
-            quae, sint quidem in commodi laborum. Rem autem eius impedit maiores praesentium laborum quos exercitationem
-            eveniet ut voluptas quas non asperiores sunt id ex provident, temporibus illo quia architecto sequi
-            assumenda. Laboriosam vero ipsa quas mollitia, sed dolores accusantium tenetur est debitis nulla aliquid
-            vel. Quaerat aliquam excepturi assumenda autem, asperiores commodi necessitatibus aspernatur eveniet,
-            architecto placeat blanditiis modi est consequatur illo quia. Id corrupti culpa eum quasi debitis nemo ipsum
-            impedit iure, temporibus, reprehenderit rerum reiciendis, dolor velit? Soluta veritatis perferendis magni
-            eos praesentium est iure qui, at libero optio architecto amet perspiciatis id! Nemo aliquam possimus sint
-            rem tempore aperiam voluptates consequuntur, explicabo non veritatis nihil veniam amet accusamus ab vel
-            quibusdam molestiae modi molestias blanditiis beatae! Consectetur distinctio quis expedita magni nostrum
-            error officia obcaecati natus, alias dolores repellat ex, reprehenderit nemo porro corporis excepturi eius
-            tempora illo harum pariatur dolor velit.</p>
+    <div class="container-fluid pt-4">
+        <div class="image_wrapper d-flex align-center justify-content-center">
+            <div class="about_image rounded-circle p-2 mb-3">
+                <img width="100" class="img-fluid rounded-circle h-100 w-100" src="../../public/img/marco-about.jpeg"
+                    alt="">
+            </div>
+        </div>
+        <section class="info_about text-center">
+            <h1 class="title_info text-white mb-0 pb-3">I'm <span class="bf_secondary">Marco
+                    Inchingoli</span>,
+                a
+                Full Stack Web
+                Developer</h1>
+            <p class="lead text-secondary mb-0 pb-3">Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Fuga dolor
+                natus
+                possimus impedit
+                omnis laboriosam dignissimos dolore quos quod ullam consequatur provident quam ex corporis, iste, animi
+                porro veniam cum? Minima et, ullam consequuntur eaque molestiae eum dolorum illum, vitae incidunt fuga
+                libero a consequatur laborum sint pariatur quisquam voluptatem autem dignissimos voluptates cumque
+                praesentium harum. Temporibus, cumque eius! Dicta fugiat laborum, quis iure, ea quas necessitatibus
+                dignissimos, excepturi fugit dolore magnam tempore! Ullam illo aut ad illum, molestiae id rem veritatis
+                enim eum aliquid fugiat animi veniam repellat pariatur assumenda laborum praesentium beatae fuga.
+                Asperiores odio dolores laudantium, odit nam est nesciunt cumque natus, deserunt aliquam minima.
+                Distinctio cupiditate dignissimos, laborum commodi odio expedita modi doloremque quo incidunt.
+                Aspernatur voluptatum sit cum ab blanditiis nulla ut laborum necessitatibus commodi, voluptas provident
+                odit exercitationem eligendi iure? Deleniti fugiat quis cumque ipsa reiciendis iure dolore officiis
+                laboriosam odio officia, reprehenderit voluptates voluptatum eius in dolorem ducimus facere voluptatibus
+                magni nisi rerum molestiae! Tempora velit quaerat est! Porro adipisci assumenda dolores quod, mollitia
+                quisquam quae aperiam vel. Similique harum soluta veniam vel, ex unde, explicabo qui culpa placeat
+                commodi delectus, omnis eligendi iusto et. Minus praesentium repellendus voluptates rerum?
+                Exercitationem, consequatur temporibus.</p>
+        </section>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+@import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;1,300&family=Satisfy&display=swap');
 
 .container-fluid {
     background-color: $bf-primary;
+
+    .about_image {
+        height: 250px;
+        width: 250px;
+        background-color: rgba(0, 0, 0, 0.2);
+
+        img {
+            object-fit: cover;
+            object-position: right top;
+        }
+    }
+
+    .title_info,
+    p {
+        font-family: 'Jost', sans-serif;
+    }
+
+    .bf_secondary {
+        color: $bf-secondary;
+        ;
+    }
 }
 </style>
